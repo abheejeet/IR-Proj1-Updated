2c03ed52c7864a3e22d4e5f5c261ba9dba3997d9
Abheejeet <abheejeet24@Gmail.com>
2021-12-06 12:18:49 -0500

    Adding changes for running Query.

--- runQuery.py
+++ runQuery.py
@@ -7,7 +7,7 @@ from googletrans import Translator
 app = Flask(__name__)
 
 
-AWS_PUBLIC_IP="18.117.83.76"
+AWS_PUBLIC_IP="3.145.29.188"
 SOLR_CORE="IRF21P4001"
 
 
@@ -21,7 +21,7 @@ URL="http://"+AWS_PUBLIC_IP+":8983/solr/"+SOLR_CORE+"/select"
 def getQueryResult_BM25(fields,query):
     print(query)
     # fields='text_en^2.5 + text_ru^2.5 + text_de^2.5 + tweet_hashtags^1'
-    PARAMS = {'defType':'dismax','qf':fields,'q':query,'fl':'id,score','wt':'json','indent':'true','rows':20}
+    PARAMS = {'defType':'dismax','qf':fields,'q':query,'fl':'id,score,tweet_text','wt':'json','indent':'true','rows':20}
 
     r = requests.get(url=URL, params=PARAMS)
     data = r.json()
@@ -52,15 +52,27 @@ def createQuery(text):
     if(detectedLang not in ['en','es','hi']):
         fields='text_en^2.5 + text_hi^2.5 + text_es^2.5 + hashtags^1.5 + mentions^1.2'
         result=getQueryResult_BM25(fields,text)
+        return result
     else:
+        result=[]
         for lang in ['en','hi','es']:
-            fields = 'text_' + lang + '^2.5 + hashtags^1.5 + mentions^1.2'
+            if lang=='en':
+                fields = 'text_en^2.5 + text_hi^0.5 + text_es^0.5  + hashtags^1.5 + mentions^1.2'
+
+            if lang=='hi':
+                fields = 'text_en^0.5 + text_hi^2.5 + hashtags^1.5 + mentions^1.2'
+
+            if lang=='es':
+                fields = 'text_en^1.0 + text_es^2.5 + hashtags^1.5 + mentions^1.2'
+
             if(detectedLang==lang):
                 query=text
             else:
                 query=convertToOtherLang(text,detectedLang,lang)
-            result = getQueryResult_BM25(fields, text)
-            print(result)
+            result.extend(getQueryResult_BM25(fields, text)['response']['docs'])
+        newlist = sorted(result, key=lambda d: d['score'],reverse=True)
+        print(newlist)
+        return newlist
 
 
 
@@ -71,6 +83,7 @@ def process_query(query):
     result=getQueryResult_BM25(query)
 
 if __name__ == '__main__':
+    createQuery("vaccine")
     # app.run(debug=True,port='8000')
     # lang=detectLang("यह बहुत कठिन क्षण है")
     # text=convertToOtherLang("यह बहुत कठिन क्षण है",lang,'hi')
